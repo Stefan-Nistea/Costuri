640ba99ffc18173a4fc24bb86db8dd06ca82467c
StefanNistea <stefannistea@outlook.com>
2025-12-05 19:21:46 +0200

    Public keys
    Added public keys for access to the project -> do not change.

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,12 @@
 // === Initialize Firebase ===
 
 const firebaseConfig = {
-  apiKey: "placeholder",
-  authDomain: "placeholder",
-  projectId: "placeholder",
-  storageBucket: "placeholder",
-  messagingSenderId: "placeholder",
-  appId: "placeholder"
+  apiKey: "AIzaSyBPlvpD9QU-uyspsbab-YDzeR3P6GturC8",
+  authDomain: "expenseflow-479418.firebaseapp.com",
+  projectId: "expenseflow-479418",
+  storageBucket: "expenseflow-479418.firebasestorage.app",
+  messagingSenderId: "889298108360",
+  appId: "1:889298108360:web:1f4d391fb6b0a7aa298afd"
 };
 
 // Global firebase object (from compat CDN)
